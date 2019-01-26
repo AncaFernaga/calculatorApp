76811691131af614cb825e06141aaec7096d634e
fernagaa <ancafernaga@yahoo.ro>
2019-01-26 23:52:39 +0200

    Initial commit

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,12 @@
+package org.fasttrackit;
+import java.util.*;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args) {
+        Calculations calculations = new Calculations();
+
+
+    }
+}
